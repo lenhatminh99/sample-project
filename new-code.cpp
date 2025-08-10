6959b11b45e3445ff099060177718f1c60476eb4
Minh Le <minhln.it@gmail.com>
2025-08-10 14:07:21 +0700

    them file code moi

---
+++ new-code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		int n, m;
+	cin >> n >> m;
+	int a[n][m];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+	}
+	int maxSum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i + 1 < n && j + 1 < m) {
+				int currentSum = a[i][j] + a[i][j + 1] + a[i + 1][j] + a[i + 1][j + 1];
+				if (currentSum > maxSum) {
+					maxSum = currentSum;
+				}
+			}
+		}
+	}
+	cout << maxSum << endl;
+	return 0;
+}
